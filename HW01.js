f4ecb45db45246162e7c8d8a10076b25166bc84e
Nikita Smirnov <nikitasmirnov16@gmail.com>
2022-07-03 11:10:38 +0300

    add comments

--- HW01.js
+++ HW01.js
@@ -1,10 +1,11 @@
+// функция повторения слова
 export function repeatWord  (word, count) {
-    let wordArr, result
-    wordArr = []
-    if (typeof word === 'string' && count >= 1 && word !== '' && isNaN(+word)) {
-            for (let i = 1; i <= count; i++) {
-                wordArr.push(word + i)
-            }   result = wordArr.join(', ')
-    } else result = 'Данные не верны'
+    let wordArr, result // объявляем переменные
+    wordArr = [] //Первую переменную делаем массивом
+    if (typeof word === 'string' && count >= 1 && word !== '' && isNaN(+word)) { // Заносим все условия при которых функция будет отрабатывать корректно
+            for (let i = 1; i <= count; i++) { //Создаем цикл
+                wordArr.push(word + i) // добавляем значения цикла в массив
+            }   result = wordArr.join(', ') // превращаем массив в строку и записываем в результат
+    } else result = 'Данные не верны' // записываем в результат если данные введенный не корректно
     return result
 }
--- tests/HW01.test.js
+++ tests/HW01.test.js
@@ -1,17 +1,21 @@
 import {repeatWord} from '../HW01.js'
 
 describe('test to repeat function', () => {
-    let correctWord, correctCount, numWord, numStrWord, minCount, emptyWord, strCount
+    let correctWord, correctCount, numWord, numStrWord, minCount, emptyWord, strCount //Объявлеяем переменные которые будем проверять в функция
+    //Корректные значения
     correctWord = 'Привет'
     correctCount = 2
+    //Некорректные значеничя
     numWord = 10
     numStrWord = '10'
     minCount = -10
     emptyWord = ''
     strCount = ''
+    // 1й тест для отработки корректного варианта
     it ('input data is correct', () => {
         expect(repeatWord(correctWord, correctCount)).toBe('Привет1, Привет2')
     })
+    // 2й для отработки некорректных вариантов
     it ('input data is not correct', () => {
         expect(repeatWord(numWord, correctCount)).toBe('Данные не верны')
         expect(repeatWord(correctWord, minCount)).toBe('Данные не верны')
